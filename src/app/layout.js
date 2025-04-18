4a61d50d5edb10626001c4de431b4f78f7c174e7
rithviksj <rithviksj@gmail.com>
2025-04-18 12:27:48 -0700

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Bihag",
+  description: "Banger playlist maker for classy souls",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
