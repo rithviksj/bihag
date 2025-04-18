f12283d3ce2aa329524048aa05651831527cfe8e
rithviksj <rithviksj@gmail.com>
2025-04-18 13:31:30 -0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <body style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
         {children}
       </body>
     </html>
