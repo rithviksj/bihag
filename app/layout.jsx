e002dca64eac15b4eff78d7373bb8196e1710dbb
Rithvik S Javgal <rithvik@rithviks-mbp.lan>
2025-04-18 18:19:22 -0700

    back to future

--- app/layout.jsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-        {children}
-      </body>
-    </html>
-  );
-}
--- app/page.jsx
+++
@@ -1,5 +0,0 @@
-import Bihag from "./Bihag";
-
-export default function Home() {
-  return <Bihag />;
-}
